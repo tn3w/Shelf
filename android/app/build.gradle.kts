8ba624ed22613410d271644e9b765915285f7d88
tn3w <tn3w@protonmail.com>
2026-09-15 19:14:52 +0200

    Bump version name to 1.0.2 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
     }
 
     flavorDimensions += "distribution"
