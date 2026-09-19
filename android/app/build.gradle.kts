b965464bffd92df93de896d9a6fcf7ee7657264f
tn3w <tn3w@protonmail.com>
2026-09-19 22:07:45 +0200

    Bump version to 1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "dev.tn3w.shelf"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     flavorDimensions += "distribution"
